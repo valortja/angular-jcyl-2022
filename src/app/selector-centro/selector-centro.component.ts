0e1192a2e01293abe0c69c8dad17a4a1334a9f0f
Javier Vallejera Ortega <valortja@jcyl.es>
2022-05-03 18:40:43 +0200

    Ejercicio 11: ------------- - Componente de selección de centro con:     * Input con event binding.     * Filtrado en código.     * Listado con directiva *ngFor.     * Selección con event binding.     * Renderizado con property data binding.

---
+++ src/app/selector-centro/selector-centro.component.ts
@@ -0,0 +1,41 @@
+import { outputAst } from '@angular/compiler';
+import { Component, OnInit, Input, Output } from '@angular/core';
+import { Centro } from '../centros/centros.component';
+
+@Component({
+  selector: 'app-selector-centro',
+  //templateUrl: './selector-centro.component.html',
+  template: `
+    <input (keyup)="buscarCentro($event)" [value]="centroSeleccionado?.nombre">
+    <ul>
+      <li *ngFor="let c of centrosFiltrados" (click)="seleccionarCentro(c)" role="button">{{c?.nombre}} </li>
+    </ul>
+  `,
+  styleUrls: ['./selector-centro.component.css']
+})
+
+export class SelectorCentroComponent implements OnInit {
+
+  centros: Centro[] = [
+    {nombre: "Ponce de León"},
+    {nombre: "San Agustín"}
+  ]
+
+  centrosFiltrados: Centro[] = [];
+  @Output()
+  centroSeleccionado?: Centro = {nombre: ''};
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  buscarCentro($event: KeyboardEvent) {
+    let target = $event.target as HTMLInputElement;
+    this.centrosFiltrados = this.centros.filter(centro => centro.nombre?.toLocaleLowerCase().includes(target.value.toLocaleLowerCase()));
+  }
+
+  seleccionarCentro(centro: Centro) {
+    this.centroSeleccionado = centro;
+  }
+}
